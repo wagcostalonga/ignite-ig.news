32889f61e077efb2adf15c30ca264c5d7ff0d8d3
Wagner <wagner.costalonga@mobato.com.br>
2021-09-08 20:29:50 -0300

    first commit

---
+++ src/components/SigInButton/index.tsx
@@ -0,0 +1,10 @@
+import { FaGithub } from 'react-icons/fa';
+
+export function SigInButton() {
+  return (
+    <button type="button">
+      <FaGithub />
+      Sign in with Github
+    </button>
+  )
+}
